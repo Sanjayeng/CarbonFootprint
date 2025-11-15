22d67fa103e272128fda0e85d146edb193c839d6
Sanjayeng <sanjay2pu@gmail.com>
2025-11-15 11:01:53 +0530

    added some files

--- sustainledger/sustainledger/settings.py
+++ sustainledger/sustainledger/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'sustainledger.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sustainledger_db',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
