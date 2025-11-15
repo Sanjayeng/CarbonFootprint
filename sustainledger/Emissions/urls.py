49c4f39ec3bed861a9a534d723e15bbf6b86b5a7
Sanjayeng <sanjay2pu@gmail.com>
2025-11-15 18:07:24 +0530

    resolved

--- sustainledger/Emissions/urls.py
+++ sustainledger/Emissions/urls.py
@@ -3,5 +3,10 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
+    path ('core/admin/', admin.site.urls),
+
     path('core/', include('Core.urls')),
+
+    path('emissions/', views.emission_index, name='emissions_index'),
+    path('emissions/add/', views.activity_add, name='activity_add'),
 ]
