a1fe7a3d30f84629eaa972fa44d9711154102b44
Sanjayeng <sanjay2pu@gmail.com>
2025-11-15 14:39:35 +0530

    core update

--- sustainledger/Core/views.py
+++ sustainledger/Core/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-
+from urllib import request
+from django.shortcuts import redirect, render
+from .models import core
 # Create your views here.
+
+  
+
 def index(request):
-    return render(request, 'Core/index.html')
+    core_objects = core.objects.all()
+    data = {'core_list': core_objects}
+    return render(request, 'Core/index.html', data)
+
+def add_core(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        location = request.POST.get('location')
+        new_core = core(name=name, location=location)
+        new_core.save()
+
+        return redirect('Core:index')
+
+    return render(request, 'Core/add.html')
+    
+   
