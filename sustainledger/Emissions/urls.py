04b6f0901948602ebfb9af7c050f3ae0dcf8757c
yashwanth <vishnuyashu684@gmail.com>
2025-11-16 11:33:16 +0530

    added edit and delete

--- sustainledger/Emissions/urls.py
+++ sustainledger/Emissions/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.emission_index, name='emission_index'),
     path('add/', views.activity_add, name='activity_add'),
+     path('edit/<int:pk>/', views.activity_edit, name='activity_edit'),
+    path('delete/<int:pk>/', views.activity_delete, name='activity_delete'),
 ]
